fccba426d92182e551d07064a5b6cfb3993168cb
Adel Kamel <adelkamel1982@gmail.com>
2019-12-11 10:27:16 +0100

    render function

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,37 @@ import { panier } from '../constants/panier'
 // PACKAGE
 import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div>
-      Test
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      afficher: false
+    }
+
+    this.panier = panier;
+  }
+
+  render() {
+
+    const elem = Object.keys(this.panier).map(id => <div>{id}</div>);
+    
+
+    return (
+      <div>
+        <button>Afficher le panier</button>
+        
+        <div>
+          {elem}
+        </div>
+
+
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
