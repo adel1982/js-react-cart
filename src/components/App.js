e3908af2ddca704ca149bfd619f152e71e02de88
Adel Kamel <adelkamel1982@gmail.com>
2019-12-11 10:03:10 +0100

    Folders organization and packages

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// CSS
+import '../css/App.css';
+
+// CONSTANT
+import { panier } from '../constants/panier'
+
+// PACKAGE
+import PropTypes from 'prop-types'
+
+function App() {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/panier.js
@@ -0,0 +1,31 @@
+export const panier = {
+	"id1" : {
+		"nom": "PC",
+		"description":"HP 16 pouces",
+		"quantite": 1,
+		"prix":799,
+		"image": "https://images.unsplash.com/photo-1548173475-56da295e8e7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+		
+	},
+	"id2" : {
+		"nom": "Livres",
+		"description":"Collection de livres anciens",
+		"quantite": 6,
+		"prix":129,
+		"image": "https://images.unsplash.com/photo-1491841651911-c44c30c34548?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+	},
+	"id3" : {
+		"nom": "Casque VR",
+		"description":"Résolution : 1080×1200",
+		"quantite": 1,
+		"prix":399,
+		"image": "https://images.unsplash.com/photo-1478416272538-5f7e51dc5400?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+	},
+	"id4" : {
+		"nom": "Haltère",
+		"description":"Kit pumps 20 kg musculation",
+		"quantite": 1,
+		"prix":69,
+		"image": "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+	}
+}
